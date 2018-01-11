9c0af405533b7716b1bf018ef3a5cc9057b2beb8
Jacob Metcalf <jacobmetcalf@Jacobs-MacBook-Air.local>
2018-01-11 10:40:23 -0700

    Initial Commit

---
+++ My Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  My Food Tracker
+//
+//  Created by Jacob Metcalf on 1/10/18.
+//  Copyright © 2018 JfMetcalf. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
